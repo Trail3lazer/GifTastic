e8c3d5c1cb890293bdc12b584e99ee3146f15fce
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-12 18:45:52 -0600

    intialize

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let topics = ["Snowboarding", "Star Wars", "Coding", "Mountaineering", "Baby Animals", "Street Racing", "Financial Independance"]
+
+for (var i = 0; i < topics.length; i++) {
+
+};
+
+$("#buttonHolder").append()
