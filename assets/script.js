48a840a7a05259952989ba42185c2cb8c346f89d
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-17 23:41:50 -0600

    fixed bugs and added ratings and static gifs

--- assets/script.js
+++ assets/script.js
@@ -8,39 +8,65 @@ var topics = [
     { 'name': "Financial Independance" }]
 var lastIdx = function () { return (topics.length - 1); };
 
+gifElementMaker = function (obj) {
+    let div = $("<div>")
+    $(div).attr("class", "float-left");
+
+    let gif = $("<img>")
+    $(gif).attr("src", obj["data-still"]);
+    $(gif).attr("data-state", 'still');
+    $(gif).attr("data-still", obj["data-still"]);
+    $(gif).attr("data-gif", obj["data-gif"]);
+    $(gif).attr("class", "gif");
+
+    let ratingHolder = $('<div>');
+
+    $(ratingHolder).text("rating: "+obj["data-rating"]);
+
+    div.append(gif);
+    div.append(ratingHolder);
+    
+
+    $("#gifHolder").append(div);
+
+};
 
 var buttonAppend = function (index) {
-    let offset = 5
+    let offset = 5;
+    let topic = topics[index];
     var btn = $("<button>");
     btn.addClass("btn-success m-2 rounded");
-    $(btn).attr("id", "topic");
-    let prop;
-    let obj = topics[index]
-    for (prop in obj) {
-        $(btn).attr(prop, $(obj).prop(prop));
-    }
-    $(btn).html(obj.name);
+    $(btn).attr("id", topic.name);
+    $(btn).html(topic.name);
     $("#buttonHolder").append(btn);
     $(btn).click(function () {
-        $("#gifHolder").empty()
-        for (let i = 0; i < 5; i++) {
-            let gif = $("<img>")
-            $(gif).attr("src", $(this).attr("data-gifUrl-" + i));
-            $("#gifHolder").append(gif);
+        $("#gifHolder").empty();
+        for (let gifI = 0; gifI < 10; gifI++) {
+            gifElementMaker(topic["gif-" + gifI]);
         }
+
+
         let more = $('<button type="button" id="more" class="btn btn-warning btn-lg">Click for more!</button>');
         $(more).click(function () {
             let howMany = 5;
             offset = offset + 5;
             $.ajax({
-                url: "https://api.giphy.com/v1/gifs/search?api_key=LK8gIICujEEO0FeSDdtIK1lyPySGR32D&q=" + obj.name + "&limit=" + howMany + "&offset=" + offset + "&rating=G&lang=en",
+                url: "https://api.giphy.com/v1/gifs/search?api_key=LK8gIICujEEO0FeSDdtIK1lyPySGR32D&q=" + topic.name + "&limit=" + howMany + "&offset=" + offset + "&rating=G&lang=en",
                 data: "data",
                 method: "GET",
                 success: function (response) {
+
                     for (let j = 0; j < response.data.length; j++) {
-                        let gif = $("<img>");
-                        $(gif).attr("src", response.data[j].images.fixed_width.url);
-                        $("#gifHolder").append(gif);
+                        topics[index]["gif-" + j] = {};
+                        let gifObj = topics[index]["gif-" + j];
+                        let active = response.data[j].images.fixed_width.url;
+                        gifObj["data-gif"] = active;
+                        let still = response.data[j].images.fixed_width_still.url;
+                        gifObj["data-still"] = still;
+                        let rating = response.data[j].rating;
+                        gifObj["data-rating"] = rating;
+
+                        gifElementMaker(gifObj);
                     }
                 }
             });
@@ -52,13 +78,21 @@ var buttonAppend = function (index) {
 
 var gifFinder = function (index) {
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=LK8gIICujEEO0FeSDdtIK1lyPySGR32D&q=" + topics[index].name + "&limit=5&offset=0&rating=G&lang=en",
+        url: "https://api.giphy.com/v1/gifs/search?api_key=LK8gIICujEEO0FeSDdtIK1lyPySGR32D&q=" + topics[index].name + "&limit=10&offset=0&rating=G&lang=en",
         data: "data",
         method: "GET",
         success: function (response) {
+
             for (let j = 0; j < response.data.length; j++) {
-                let gifObj = response.data[j].images.fixed_width.url;
-                topics[index]["data-gifUrl-" + j] = gifObj;
+                topics[index]["gif-" + j] = {};
+                let gifObj = topics[index]["gif-" + j];
+                let active = response.data[j].images.fixed_width.url;
+                gifObj["data-gif"] = active;
+                let still = response.data[j].images.fixed_width_still.url;
+                gifObj["data-still"] = still;
+                let rating = response.data[j].rating;
+                gifObj["data-rating"] = rating;
+                console.log(gifObj)
             }
             buttonAppend(index);
         }
@@ -75,6 +109,17 @@ var newTopic = function () {
     topics.push(newTopic);
     gifFinder(lastIdx());
 };
+
+$('#gifHolder').on("click",".gif",function() {
+    console.log(this)
+    if ($(this).attr('data-state')==="active"){
+        $(this).attr('src',$(this).attr('data-still'));
+        $(this).attr('data-state',"still");
+    } else {
+        $(this).attr('src', $(this).attr('data-gif'));
+        $(this).attr('data-state',"active");
+    }
+});
 
 $("#searchButton").click(function () {
     newTopic(lastIdx());
